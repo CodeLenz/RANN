107048579676b12f8b4dd302999fc4d0cc5ac2fc
Rodrigo Polidoro <rodrigo.hpolidoro@gmail.com>
2025-08-26 19:39:50 -0300

    Revisado main.jl para uso do módulo.

--- src/main.jl
+++ src/main.jl
@@ -2,26 +2,6 @@
 # Algoritmo para cálculo de rede neural
 #
 
-# Bibliotecas  
-using StatsFuns # função sigmoide (logística)
-using StatsBase # Função Sample
-using LinearAlgebra # cálculo de norma
-using Plots # Gráficos
-using Random; Random.seed!(1234) # define um seed para as variáveis aleatórias 
-using Enzyme # diferenciação automática
-using DelimitedFiles # Leitura dos arquivos .csv
-using CodecZlib # Leitura de arquivos comprimidos (.gz)
-using ProgressMeter # Barra de progresso ao rodar o código
-
-# Adiciona demais arquivos do programa
-include("struct_rede.jl")
-include("dados_treino.jl")
-include("objetivo.jl")
-include("RNA.jl")
-include("adam.jl")
-include("ativ.jl")
-include("acuracia.jl")
-
 # Função principal do código
 function main(topologia::Vector{Int64}, ativ::Tuple)
 
