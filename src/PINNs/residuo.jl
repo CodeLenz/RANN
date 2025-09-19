c1c4ad7b7d1f8db25afc306bb40332105372879a
Eduardo Lenz <eduardobarplenz@gmail.com>
2025-09-19 13:21:58 -0300

    Testes retirando x como constante
    Ainda não está funcionando na derivada em relação aos x (parâmetros da rede)

--- src/PINNs/residuo.jl
+++ src/PINNs/residuo.jl
@@ -22,7 +22,7 @@ end
 # em que x é o vetor com os parâmetros da rede e t é o tempo
 # variável independente da EDO
 #
-function residuo(x::Vector, tempo::Float64)
+function residuo(x::Vector, tempo::Vector{Float64})::Float64
 
     # Dados da EDO
     m = 1.0
@@ -33,9 +33,9 @@ function residuo(x::Vector, tempo::Float64)
 
     # Derivadas da função u
     u,du,du2 = Derivadas(resposta,tempo,x)
-    
+  
     # O resíduo no tempo t, para a rede x será 
-    r = m*du2[1] + c*du[1] + k*u - f(tempo)
+    r = m*du2[1] + c*du[1] + k*u - f(tempo[1])
      
 end
 
@@ -43,27 +43,28 @@ end
 # Calcula a resposta, a primeira derivada e a segunda derivada em 
 # relação ao tempo para um conjunto x de parâmetros fixos da rede
 #
-function Derivadas(u::Function,tempo::Float64,x::Vector)
+function Derivadas(u::Function,tempo::Vector{Float64},x::Vector)
     
     # Aloca as derivadas
     du = zeros(1)
     du2 = zeros(1)
 
-    # Passa o tempo para um vetor
-    t = [tempo]
 
     # Driver para calcular a derivada em relação a t
     function derivada!(x::Vector,saida::Vector,ponto::Vector)
+            # Criei uma variável auxiliar aqui...não vamos usar
+            teste = copy(x)
+            fill!(teste,0)
             Enzyme.autodiff(
                         Enzyme.set_runtime_activity(Enzyme.Reverse),
                         u,
-                        Const(x),
-                        Duplicated(ponto, saida)
+                        Enzyme.Duplicated(x,teste),
+                        Enzyme.Duplicated(ponto, saida)
                     )
     end
    
     # Calcula a primeira derivada
-    derivada!(x,du,t)
+    derivada!(x,du,tempo)
 
     # Multiplica a Hessiana pelo vetor [1]
     v = ones(1)
@@ -71,20 +72,38 @@ function Derivadas(u::Function,tempo::Float64,x::Vector)
     # Aloca a saída
     du2 = zeros(1)
 
+    # Mais uma alocação temporária
+    ddd = copy(x)  
+    fill!(ddd,0)
+
     # Calcula a segunda derivada em relação ao tempo
     Enzyme.autodiff(Enzyme.Forward, 
                     derivada!,
-                    Const(x),
+                    Enzyme.Duplicated(x,ddd),
                     Enzyme.BatchDuplicated(zeros(1), (du2,)), 
-                    Enzyme.BatchDuplicated(t, (v,))) 
+                    Enzyme.BatchDuplicated(tempo, (v,))) 
 
 
-    return u(x,t), du, du2
+    return u(x,tempo), du, du2
 
 end
 
-#
-# x = rand(5)
-# r = [residuo(x,t) for t in 0:0.1:1.0]
-# norm(r)
-#
+ # Calcula os resíduos
+ x = rand(5)
+ r = [residuo(x,[t]) for t in 0:0.1:1.0]
+ norm(r)
+
+ #
+ # O problema que estamos tendo é derivada em relação a x (parâmetros da rede)
+ # considerando que já estamos utilizando o Enzyme para calcular as derivadas 
+ # da EDO
+ 
+ dx = ones(5)
+ ponto = [1.0]
+ dponto = [0.0]
+ Enzyme.autodiff(
+                        Enzyme.set_runtime_activity(Enzyme.Reverse),
+                        residuo,
+                        Duplicated(x,dx),
+                        Duplicated(ponto,dponto)
+                    )
